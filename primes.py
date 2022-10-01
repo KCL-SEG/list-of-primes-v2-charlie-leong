885b1ce4d36ab8eb5b691c015533e0e857913c96
Charlotte Hoi Yi Leong <charlotte.leong@kcl.ac.uk>
2022-10-01 21:55:15 +0100

    read instructions properly, amendments made accordingly

--- primes.py
+++ primes.py
@@ -3,16 +3,16 @@
 
 def primes(number_of_primes):
     list = []
-    try:
-        num = 2
-        while len(list) < number_of_primes:
-            flag = True
-            for i in range(2, num):
-                if(num % i == 0):
-                    flag = False
-            if( flag):
-                list.append(num)
-            num += 1
-        return list
-    except ValueError:
-        print(f"yikes dude")
+    if x<=0:
+        raise ValueError("should be positive")
+    num = 2
+    while len(list) < number_of_primes:
+        flag = True
+        for i in range(2, num):
+            if(num % i == 0):
+                flag = False
+        if( flag):
+            list.append(num)
+        num += 1
+    return list
+        
